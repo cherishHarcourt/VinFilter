b1d7acc718eb84d9579f3330bbe8ea983275ddb3
cherishHarcourt <cherish.harcourt@gmail.com>
2025-11-07 15:09:05 +0100

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./Navbar.css"
+import logo from "../../assets/images/icons8-camera-100.png"
+
+const Navbar = () => {
+  return (
+    <div className="navbox">
+        <div className="logo">
+            <img src={logo} alt="logo" />
+            <h3>Vinfilter</h3>
+        </div>
+
+        <div className="navoptions">
+            <h3>Home</h3>
+            <h3>Features</h3>
+            <h3>Guide</h3>
+            <h3>Reviews</h3>
+        </div>
+
+        <div className="sidenav">
+            <h3>Download</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
